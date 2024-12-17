c8566241107f6208479ec0d4cd4b025468fe98d1
abdulghofur12 <ghofursans12@gmail.com>
2024-12-17 09:08:46 +0700

    penambahan anagram

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -1,3 +1,23 @@
-public class Anagram {
+import java.util.Arrays;
+public class Anagram{
+    public static boolean AreAnagram(String strA, String strB) {
+        strA = strA.toLowerCase();
+        strB = strB.toLowerCase();
+        if (strA.length() != strB.length()) {
+            return false;
+        }
+        char[] charArrayA = strA.toCharArray();
+        char[] charArrayB = strB.toCharArray();
+        Arrays.sort(charArrayA);
+        Arrays.sort(charArrayB);
+        return Arrays.equals(charArrayA, charArrayB);
+    }
+
+    public static void main(String[] args) {
+        String stringA = "cat";
+        String stringB = "tac";
+        boolean result = AreAnagram(stringA, stringB);
+        System.out.println("Apakah \"" + stringA + "\" dan \"" + stringB + "\" adalah anagram? " + result);
+    }
 
 }
